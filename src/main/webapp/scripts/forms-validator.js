a793109cc656a946ab2690c8bf0a0197ea1ed165
amiraElmergawy <amiraelmergawy@gmail.com>
2022-03-24 20:10:18 +0200

    Update forms-validator.js

--- src/main/webapp/scripts/forms-validator.js
+++ src/main/webapp/scripts/forms-validator.js
@@ -1,14 +1,10 @@
-function validateInputs() {
-    console.log("hello");
-}
-
 const setError = (element, message) => {
     const inputControl = element.parentElement;
     const errorDisplay = inputControl.querySelector('.error');
 
     errorDisplay.innerText = message;
     inputControl.classList.add('error');
-    inputControl.classList.remove('success')
+    // inputControl.classList.remove('success')
 }
 
 const setSuccess = element => {
@@ -16,7 +12,7 @@ const setSuccess = element => {
     const errorDisplay = inputControl.querySelector('.error');
 
     errorDisplay.innerText = '';
-    inputControl.classList.add('success');
+    // inputControl.classList.add('success');
     inputControl.classList.remove('error');
 };
 
@@ -25,19 +21,25 @@ const validateEmptyInputs = element => {
 
     if (elmentValue === '') {
         setError(element, 'This field is required');
+        return false;
     } else {
         setSuccess(element);
+        return true;
     }
+
 };
 
 const validateEmailInput = emailElement => {
 
     if (emailElement.value === '') {
         setError(emailElement, 'Email is required');
+        return false;
     } else if (!isValidEmail(emailElement.value)) {
         setError(emailElement, 'Provide a valid email address');
+        return false;
     } else {
         setSuccess(emailElement);
+        return true;
     }
 }
 
@@ -45,10 +47,13 @@ const validatePasswordInput = passwordElment => {
 
     if (passwordElment.value === '') {
         setError(passwordElment, 'Password is required');
+        return false;
     } else if (passwordElment.value.length < 8) {
         setError(passwordElment, 'Password must be at least 8 character.')
+        return false;
     } else {
         setSuccess(passwordElment);
+        return true;
     }
 }
 
@@ -56,10 +61,13 @@ const checkPasswordInputsSimilarity = (passwordElement, confirmPasswordElement) 
 
     if (confirmPasswordElement.value === '') {
         setError(confirmPasswordElement, 'Please confirm your password');
+        return false;
     } else if (confirmPasswordElement.value !== passwordElement.value) {
         setError(confirmPasswordElement, "Passwords doesn't match");
+        return false;
     } else {
         setSuccess(confirmPasswordElement);
+        return true;
     }
 
 }
