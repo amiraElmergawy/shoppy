e3f739b8730ab73d6f1e10527ff622d9199adfbe
Abdelazizeldahshan <abdelazizeldahshan5@gmail.com>
2022-03-26 16:33:37 +0200

    create user Entity and address entity and role enum

--- src/main/java/gov/iti/jets/shoppy/repository/entity/UserEntity.java
+++ src/main/java/gov/iti/jets/shoppy/repository/entity/UserEntity.java
@@ -12,4 +12,14 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    private String username;
+    private String email;
+    private String password;
+    private String interests;
+    private boolean isMale;
+    private String dateOfBirth;
+    private String jobTitle;
+    private double creditLimit;
+    private AddressEntity address;
+    private Role role;
 }
