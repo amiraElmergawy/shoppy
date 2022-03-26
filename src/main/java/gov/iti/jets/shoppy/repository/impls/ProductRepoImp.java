1dc8ff5e37d4586b279b8a3c7a2b4192c40d7867
heshmAhmed <heshamahmed22226.ha@gmail.com>
2022-03-26 18:24:06 +0200

    added product repo

--- src/main/java/gov/iti/jets/shoppy/repository/impls/ProductRepoImp.java
+++ src/main/java/gov/iti/jets/shoppy/repository/impls/ProductRepoImp.java
@@ -1,6 +1,27 @@
 package gov.iti.jets.shoppy.repository.impls;
 
+import gov.iti.jets.shoppy.repository.entity.ProductEntity;
 import gov.iti.jets.shoppy.repository.interfaces.ProductRepo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class ProductRepoImp implements ProductRepo {
+    private final List<ProductEntity> productEntityList = new ArrayList<>();
+    private final static ProductRepoImp productImpl = new ProductRepoImp();
+    private ProductRepoImp() {}
+    public static ProductRepoImp getInstance() {
+        return productImpl;
+    }
+
+    @Override
+    public List<ProductEntity> getAllProducts() {
+        return this.productEntityList;
+    }
+
+    @Override
+    public Optional<ProductEntity> findProductById(long id) {
+        return  productEntityList.stream().filter(productEntity -> productEntity.getId() == id).findFirst();
+    }
 }
