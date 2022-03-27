9ceb66d1f9d807c6fcc3bd7746167d9f7d6152b9
amiraElmergawy <amiraelmergawy@gmail.com>
2022-03-28 00:38:40 +0200

    Update DomainFacade.java

--- src/main/java/gov/iti/jets/shoppy/service/DomainFacade.java
+++ src/main/java/gov/iti/jets/shoppy/service/DomainFacade.java
@@ -1,27 +1,11 @@
 package gov.iti.jets.shoppy.service;
 
 import gov.iti.jets.shoppy.presentation.helpers.LoginViewHelper;
-import gov.iti.jets.shoppy.repository.entity.UserEntity;
-import gov.iti.jets.shoppy.repository.util.RepoFactory;
-import gov.iti.jets.shoppy.service.interfaces.UserMapper;
-
-import java.util.Optional;
+import gov.iti.jets.shoppy.service.util.ServiceFactory;
 
 public class DomainFacade {
 
-    public LoginViewHelper sigin(String email, String password){
-        LoginViewHelper loginViewHelper = LoginViewHelper.getInstance();
-        Optional<UserEntity> userEntityResult = RepoFactory.INSTANCE.getUserRepo().findUser(email, password);
-        userEntityResult.ifPresentOrElse(
-                (value) -> {
-                    System.out.println("Value is present, its: "+ value);
-                    loginViewHelper.setUserDto(UserMapper.INSTANCE.userEntityToUserDto(value));
-                    },
-                () -> {
-                    System.out.println("Value is empty");
-                    loginViewHelper.setError("Invalid email or password");
-                });
-
-        return loginViewHelper;
+    public LoginViewHelper signin(String email, String password){
+        return ServiceFactory.INSTANCE.getAuthService().signin(email, password);
     }
 }
