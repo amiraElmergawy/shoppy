5fd98499f73c6e194668caa8aa77b15f59c65b2d
Abdelazizeldahshan <abdelazizeldahshan5@gmail.com>
2022-03-28 00:43:08 +0200

    login feature senario 2

--- src/main/java/gov/iti/jets/shoppy/service/DomainFacade.java
+++ src/main/java/gov/iti/jets/shoppy/service/DomainFacade.java
@@ -5,6 +5,13 @@ import gov.iti.jets.shoppy.service.util.ServiceFactory;
 
 public class DomainFacade {
 
+    private static DomainFacade domainFacade = new DomainFacade();
+
+    private DomainFacade(){}
+
+    public static DomainFacade getInstance(){
+        return domainFacade;
+    }
     public LoginViewHelper signin(String email, String password){
         return ServiceFactory.INSTANCE.getAuthService().signin(email, password);
     }
