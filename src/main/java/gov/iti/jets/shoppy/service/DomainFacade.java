adfd161955d1fb330cd0b2caa0a9e1ba3faf6e13
amiraElmergawy <amiraelmergawy@gmail.com>
2022-03-27 23:14:18 +0200

    Add signin method to domain facade

--- src/main/java/gov/iti/jets/shoppy/service/DomainFacade.java
+++ src/main/java/gov/iti/jets/shoppy/service/DomainFacade.java
@@ -1,4 +1,27 @@
 package gov.iti.jets.shoppy.service;
 
+import gov.iti.jets.shoppy.presentation.helpers.LoginViewHelper;
+import gov.iti.jets.shoppy.repository.entity.UserEntity;
+import gov.iti.jets.shoppy.repository.util.RepoFactory;
+import gov.iti.jets.shoppy.service.interfaces.UserMapper;
+
+import java.util.Optional;
+
 public class DomainFacade {
+
+    public LoginViewHelper sigin(String email, String password){
+        LoginViewHelper loginViewHelper = LoginViewHelper.getInstance();
+        Optional<UserEntity> userEntityResult = RepoFactory.INSTANCE.getUserRepo().findUser(email, password);
+        userEntityResult.ifPresentOrElse(
+                (value) -> {
+                    System.out.println("Value is present, its: "+ value);
+                    loginViewHelper.setUserDto(UserMapper.INSTANCE.userEntityToUserDto(value));
+                    },
+                () -> {
+                    System.out.println("Value is empty");
+                    loginViewHelper.setError("Invalid email or password");
+                });
+
+        return loginViewHelper;
+    }
 }
