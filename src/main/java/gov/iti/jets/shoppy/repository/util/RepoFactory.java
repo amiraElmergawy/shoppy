be0f96733140b57b35026aa40933707f803f7bf8
amiraElmergawy <amiraelmergawy@gmail.com>
2022-04-03 20:50:34 +0200

    Update RepoFactory.java

--- src/main/java/gov/iti/jets/shoppy/repository/util/RepoFactory.java
+++ src/main/java/gov/iti/jets/shoppy/repository/util/RepoFactory.java
@@ -4,13 +4,14 @@ import gov.iti.jets.shoppy.repository.impls.ProductRepoImp;
 import gov.iti.jets.shoppy.repository.impls.UserRepoImpl;
 import gov.iti.jets.shoppy.repository.interfaces.ProductRepo;
 import gov.iti.jets.shoppy.repository.interfaces.UserRepo;
+import jakarta.persistence.EntityManager;
 
 public enum RepoFactory {
     INSTANCE;
-    private final UserRepo userRepo = UserRepoImpl.getInstance();
+//    private final UserRepo userRepo = UserRepoImpl.getInstance();
     private final ProductRepo productRepo = ProductRepoImp.getInstance();
-    public UserRepo getUserRepo() {
-        return userRepo;
+    public UserRepo getUserRepo(EntityManager entityManager) {
+        return new UserRepoImpl(entityManager);
     }
 
     public ProductRepo getProductRepo() {
