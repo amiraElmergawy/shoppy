993899d3d8a486e073a36c2dfb9eb0e8be557c95
amiraElmergawy <amiraelmergawy@gmail.com>
2022-04-05 01:20:09 +0200

    Update AuthServiceImpl.java

--- src/main/java/gov/iti/jets/shoppy/service/impls/AuthServiceImpl.java
+++ src/main/java/gov/iti/jets/shoppy/service/impls/AuthServiceImpl.java
@@ -1,11 +1,13 @@
 package gov.iti.jets.shoppy.service.impls;
 
 import gov.iti.jets.shoppy.presentation.helpers.LoginViewHelper;
+import gov.iti.jets.shoppy.repository.entity.AdminEntity;
+import gov.iti.jets.shoppy.repository.entity.CustomerEntity;
 import gov.iti.jets.shoppy.repository.entity.UserEntity;
 import gov.iti.jets.shoppy.repository.util.RepoFactory;
 import gov.iti.jets.shoppy.service.interfaces.AuthService;
 import gov.iti.jets.shoppy.service.mappers.AdminMapper;
-import gov.iti.jets.shoppy.service.mappers.UserMapper;
+import gov.iti.jets.shoppy.service.mappers.CustomerMapper;
 import gov.iti.jets.shoppy.service.util.HashManager;
 import jakarta.persistence.EntityManager;
 
@@ -25,9 +27,9 @@ public class AuthServiceImpl implements AuthService {
                     String entityType = value.getClass().getName().substring(value.getClass().getName().lastIndexOf(".")+1);
                     System.out.println("Value is present, its: "+ value);
                     if(entityType.equals("AdminEntity")){
-                        loginViewHelper.setAdminDto(UserMapper.INSTANCE.entityToAdminDto(value));
+                        loginViewHelper.setAdminDto(AdminMapper.INSTANCE.adminEntityToDto((AdminEntity) value));
                     }else {
-                        loginViewHelper.setCustomerDto(UserMapper.INSTANCE.entityToCustomerDto(value));
+                        loginViewHelper.setCustomerDto(CustomerMapper.INSTANCE.customerEntityToDto((CustomerEntity) value));
                     }
                 },
                 () -> {
