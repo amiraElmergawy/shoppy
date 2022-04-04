b12f0beeab5704aaa19d25068b7581fd7c13cea8
Hala Tarek <halatarek80@gmail.com>
2022-04-04 21:43:04 +0200

    Update Domain Facade

--- src/main/java/gov/iti/jets/shoppy/service/DomainFacade.java
+++ src/main/java/gov/iti/jets/shoppy/service/DomainFacade.java
@@ -1,7 +1,9 @@
 package gov.iti.jets.shoppy.service;
 
+import gov.iti.jets.shoppy.presentation.helpers.HomeViewHelper;
 import gov.iti.jets.shoppy.presentation.helpers.LoginViewHelper;
 import gov.iti.jets.shoppy.service.interfaces.AuthService;
+import gov.iti.jets.shoppy.service.interfaces.ProductService;
 import gov.iti.jets.shoppy.service.util.ServiceFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -11,6 +13,7 @@ public class DomainFacade {
     private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("shoppy");
 
     private final AuthService authService = ServiceFactory.INSTANCE.getAuthService();
+    private final ProductService productService = ServiceFactory.INSTANCE.getProductService();
 
     private static DomainFacade domainFacade = new DomainFacade();
     private DomainFacade(){}
@@ -18,8 +21,13 @@ public class DomainFacade {
     public static DomainFacade getInstance(){
         return domainFacade;
     }
+
     public LoginViewHelper signIn(String email, String password){
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         return authService.signIn(email, password, entityManager);
+    }
+    public  HomeViewHelper showProducts(){
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        return productService.getProducts(entityManager);
     }
 }
