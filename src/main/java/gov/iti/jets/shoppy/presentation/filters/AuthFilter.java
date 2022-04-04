19b1e5e49fbdf47f15b6829cacd55bce06aea583
amiraElmergawy <amiraelmergawy@gmail.com>
2022-04-04 03:12:26 +0200

    Update AuthFilter.java
    Add possible url list

--- src/main/java/gov/iti/jets/shoppy/presentation/filters/AuthFilter.java
+++ src/main/java/gov/iti/jets/shoppy/presentation/filters/AuthFilter.java
@@ -11,6 +11,23 @@ public class AuthFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        urlList.add("");
+        urlList.add("login");
+        urlList.add("signIn");
+        urlList.add("home");
+        urlList.add("register");
+        urlList.add("signUp");
+        urlList.add("about-us");
+        urlList.add("product-detail");
+        urlList.add("product-cart");
+        urlList.add("admin/add-product");
+        urlList.add("admin/update-product");
+        urlList.add("admin/products");
+        urlList.add("admin/product-details");
+        urlList.add("admin/product");
+        urlList.add("admin/customers");
+        urlList.add("admin/orders");
+        urlList.add("admin/dashboard");
         Filter.super.init(filterConfig);
     }
 
