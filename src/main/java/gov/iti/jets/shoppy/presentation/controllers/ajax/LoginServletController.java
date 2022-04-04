9e447e2c8fe5edf624fedfd877342f1acee1decb
amiraElmergawy <amiraelmergawy@gmail.com>
2022-04-04 05:51:52 +0200

    Update LoginServletController.java

--- src/main/java/gov/iti/jets/shoppy/presentation/controllers/ajax/LoginServletController.java
+++ src/main/java/gov/iti/jets/shoppy/presentation/controllers/ajax/LoginServletController.java
@@ -1,5 +1,6 @@
 package gov.iti.jets.shoppy.presentation.controllers.ajax;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,8 +9,18 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet (name = "SigninServlet" , value = "/signin")
+@WebServlet (name = "LoginServletController" , value = "/login")
 public class LoginServletController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("WEB-INF/views/customer/auth-forms.jsp");
+        try {
+            rd.include(req,resp);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
