1406b6ad6dd4725294d8c1b6ffde4b9f5e867365
amiraElmergawy <amiraelmergawy@gmail.com>
2022-04-05 02:35:54 +0200

    Update AuthFilter.java

--- src/main/java/gov/iti/jets/shoppy/presentation/filters/AuthFilter.java
+++ src/main/java/gov/iti/jets/shoppy/presentation/filters/AuthFilter.java
@@ -2,6 +2,7 @@ package gov.iti.jets.shoppy.presentation.filters;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -34,9 +35,21 @@ public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 //       HttpServletRequest request = (HttpServletRequest) servletRequest;
-//        System.out.println("url "+request.getRequestURL());
+//        System.out.println("url "+((HttpServletRequest) servletRequest).getRequestURL());
 //        System.out.println("path "+request.getContextPath());
+        HttpSession httpSession = ((HttpServletRequest) servletRequest).getSession(false);
+        if (httpSession != null){
+            System.out.println("from auth filter, http session is not null");
+            String role = (String) httpSession.getAttribute("role");
+            if(role != null){
+                System.out.println("role not null: "+role);
+                if (role.equals("admin"))
+                    System.out.println("this is the admin");
+            }
+        }
+
         filterChain.doFilter(servletRequest,servletResponse);
+
     }
 
     @Override
