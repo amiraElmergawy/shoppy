0c13b5e5ca6ac45ade4ac4589cfcb043100904b5
amiraElmergawy <amiraelmergawy@gmail.com>
2022-04-10 06:46:42 +0200

    Update RegisterServletController.java

--- src/main/java/gov/iti/jets/shoppy/presentation/controllers/ajax/RegisterServletController.java
+++ src/main/java/gov/iti/jets/shoppy/presentation/controllers/ajax/RegisterServletController.java
@@ -1,5 +1,8 @@
 package gov.iti.jets.shoppy.presentation.controllers.ajax;
 
+import gov.iti.jets.shoppy.service.DomainFacade;
+import gov.iti.jets.shoppy.service.dtos.CustomerDto;
+import gov.iti.jets.shoppy.service.util.HashManager;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,6 +11,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
 
 @WebServlet (name = "RegisterServletController" , value = "/register")
 public class RegisterServletController extends HttpServlet {
@@ -23,8 +30,31 @@ public class RegisterServletController extends HttpServlet {
     }
     @Override
     protected  void doPost(HttpServletRequest req, HttpServletResponse resp)  throws ServletException, IOException{
-
-
+        String name= req.getParameter("name").trim();
+        String email= req.getParameter("email").trim();
+        String password= req.getParameter("password").trim();
+        String birthDate= req.getParameter("birthDate").trim();
+        String favorite= req.getParameter("favorite").trim();
+        String gender = req.getParameter("gender").trim();
+        boolean isMale = gender == "male"? true : false;
+        Date formattedDOB = new Date();
+        try {
+            formattedDOB = new SimpleDateFormat("yyyy-MM-dd").parse(birthDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        CustomerDto customerDto = CustomerDto.builder()
+                .username(name)
+                .email(email)
+                .password(HashManager.INSTANCE.generateSecurePassword(password))
+                .dateOfBirth(formattedDOB)
+                .isMale(isMale)
+                .interests(favorite).build();
+        if(DomainFacade.getInstance().signUp(customerDto))
+            resp.sendRedirect("login");
+        else {
+            resp.sendRedirect("register?emailValidation=false");
+        }
     }
 
 }
