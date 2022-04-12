b49fcc9a6137bb3293b48bde846dd5f086185177
Sally <sallyelbanawany1998@gmail.com>
2022-04-13 01:05:52 +0200

    add profile service

--- src/main/java/gov/iti/jets/shoppy/service/util/ServiceFactory.java
+++ src/main/java/gov/iti/jets/shoppy/service/util/ServiceFactory.java
@@ -2,17 +2,22 @@ package gov.iti.jets.shoppy.service.util;
 
 import gov.iti.jets.shoppy.service.impls.AuthServiceImpl;
 import gov.iti.jets.shoppy.service.impls.ProductServiceImpl;
+import gov.iti.jets.shoppy.service.impls.ProfileServiceImpl;
 import gov.iti.jets.shoppy.service.interfaces.AuthService;
 import gov.iti.jets.shoppy.service.interfaces.ProductService;
+import gov.iti.jets.shoppy.service.interfaces.ProfileService;
 
 public enum ServiceFactory {
     INSTANCE;
 
     private AuthService authService = new AuthServiceImpl();
     private ProductService productService = new ProductServiceImpl();
+    private ProfileService profileService = new ProfileServiceImpl();
 
     public AuthService getAuthService() {
         return authService;
     }
     public ProductService getProductService() {return productService;}
+    public ProfileService getProfileService(){return profileService;}
+
 }
