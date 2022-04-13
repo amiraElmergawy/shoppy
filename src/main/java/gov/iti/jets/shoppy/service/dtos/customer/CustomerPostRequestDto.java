94ca9a7b1167461c1e9bdbfcb51332cc5376d5bd
amiraElmergawy <amiraelmergawy@gmail.com>
2022-04-14 01:07:18 +0200

    Update CustomerPostRequestDto.java

--- src/main/java/gov/iti/jets/shoppy/service/dtos/customer/CustomerPostRequestDto.java
+++ src/main/java/gov/iti/jets/shoppy/service/dtos/customer/CustomerPostRequestDto.java
@@ -1,11 +1,13 @@
 package gov.iti.jets.shoppy.service.dtos.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
 @Builder
 public class CustomerPostRequestDto {
     private String username;
