5b39cd167daafe2703d64b76d45df31612124836
heshmAhmed <heshamahmed22226.ha@gmail.com>
2022-04-14 22:32:39 +0200

    add customer dto and mapper

--- src/main/java/gov/iti/jets/shoppy/service/mappers/CustomerMapper.java
+++ src/main/java/gov/iti/jets/shoppy/service/mappers/CustomerMapper.java
@@ -3,6 +3,7 @@ package gov.iti.jets.shoppy.service.mappers;
 import gov.iti.jets.shoppy.repository.entity.CustomerEntity;
 import gov.iti.jets.shoppy.service.dtos.CustomerDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
