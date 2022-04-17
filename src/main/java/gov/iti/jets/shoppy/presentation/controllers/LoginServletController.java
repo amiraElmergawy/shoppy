f21fa69abc02d7fbb752584ea85b7556b43a386c
amiraElmergawy <amiraelmergawy@gmail.com>
2022-04-18 00:52:21 +0200

    refactor

--- src/main/java/gov/iti/jets/shoppy/presentation/controllers/LoginServletController.java
+++ src/main/java/gov/iti/jets/shoppy/presentation/controllers/LoginServletController.java
@@ -22,26 +22,11 @@ public class LoginServletController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        if(req.getSession(false) != null && req.getSession(false).getAttribute("userRole") != null){
-            if (req.getSession(false).getAttribute("userRole").equals(Role.CUSTOMER) ){
-                resp.sendRedirect("home");
-            }else{
-                resp.sendRedirect("dashboard");
-            }
-        }else {
-//            if (req.getParameter("loginBoolean") != null) {
-//                resp.getWriter().println("Login Failed , invalid email or password , Please Try Again");
-//            }
-//            if (req.getParameter("notValid") != null) {
-//                req.setAttribute("error", "invalid format email or password");
-//                req.getRequestDispatcher("WEB-INF/views/customer/auth-forms.jsp").forward(req, resp);
-//            }
-            RequestDispatcher rd = req.getRequestDispatcher("WEB-INF/views/customer/auth-forms.jsp");
-            try {
-                rd.include(req,resp);
-            } catch (ServletException e) {
-                e.printStackTrace();
-            }
+        RequestDispatcher rd = req.getRequestDispatcher("WEB-INF/views/customer/auth-forms.jsp");
+        try {
+            rd.include(req,resp);
+        } catch (ServletException e) {
+           e.printStackTrace();
         }
     }
 
@@ -51,24 +36,6 @@ public class LoginServletController extends HttpServlet {
         String password = req.getParameter("password");
         String remember = req.getParameter("remember");
         LoginViewHelper loginViewHelper = DomainFacade.getInstance().signIn(email, password);
-//        PrintWriter writer =resp.getWriter();
-//        System.out.println("Customer DTO "+loginViewHelper.getCustomerDto());
-//        System.out.println("Admin Dto " + loginViewHelper.getAdminDto());
-//        System.out.println("Error "+ loginViewHelper.getError());
-//        if (loginViewHelper.getCustomerDto() != null){
-//            httpSession.setAttribute("role", "customer");
-//            httpSession.setAttribute("data", loginViewHelper.getCustomerDto());
-//            resp.sendRedirect("home");
-//        } else if(loginViewHelper.getAdminDto() != null){
-//            httpSession.setAttribute("role", "admin");
-//            httpSession.setAttribute("data", loginViewHelper.getAdminDto());
-//            resp.sendRedirect("dashboard");
-//        }
-//        if(loginViewHelper.getError() != null){
-//            req.setAttribute("helper", loginViewHelper);
-//            req.getRequestDispatcher("WEB-INF/views/customer/auth-forms.jsp").forward(req, resp);
-//        }
-
         if(loginViewHelper.getError() == null) {
             sessionManager.createSession(req, loginViewHelper);
             if (remember != null)
@@ -80,7 +47,7 @@ public class LoginServletController extends HttpServlet {
         }
         else {
             req.setAttribute("error", loginViewHelper.getError());
-            req.getRequestDispatcher("WEB-INF/views/customer/auth-forms.jsp").forward(req, resp);
+            req.getRequestDispatcher("login").forward(req, resp);
         }
     }
 }
