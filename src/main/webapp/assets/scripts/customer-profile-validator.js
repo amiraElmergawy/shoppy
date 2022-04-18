7dab4a7ab817a44b22d54e68d4284653cf550dbc
Sally <sallyelbanawany1998@gmail.com>
2022-04-18 02:09:51 +0200

    edit customer validation

--- src/main/webapp/assets/scripts/customer-profile-validator.js
+++ src/main/webapp/assets/scripts/customer-profile-validator.js
@@ -8,8 +8,10 @@ const cityElement = document.getElementById("city");
 const streetElement = document.getElementById("street");
 const floorElement = document.getElementById("floor-number");
 const buildingElement = document.getElementById("building-number");
+const emailEelement=document.getElementById("email");
+
 const editButton= document.getElementById("edit");
-const submitButton = document.getElementById("submit");
+const submitButton = document.getElementById("save");
 // form.addEventListener('submit', e => {
 //     e.preventDefault();
 //     validateNameInput(nameElement);
@@ -23,6 +25,7 @@ const submitButton = document.getElementById("submit");
 // })
 editButton.addEventListener('click',e=>{
     nameElement.removeAttribute("disabled");
+    emailEelement.removeAttribute("disabled");
     jopElement.removeAttribute("disabled");
     dateElement.removeAttribute("disabled");
     genderElement.removeAttribute("disabled");
@@ -35,41 +38,53 @@ editButton.addEventListener('click',e=>{
 
  })
 
-submitButton.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
+    console.log("%%%%%%%%%form_______");
     e.preventDefault();
-    const validName=validateNameInput(nameElement);
-    const validEmail=validateEmailInput(emailEelement);
+    const validName=validateEmptyInputs(nameElement);
     const validBirthDate=validateEmptyInputs(dateElement);
     const validStreet=validateEmptyInputs(streetElement);
     const validBuildingNumber=validateEmptyInputs(buildingElement);
     const validFloorNumber=validateEmptyInputs(floorElement);
     const validJob=validateEmptyInputs(jopElement);
     const validCity=validateEmptyInputs(cityElement);
+    console.log(validName);
+    console.log(validBirthDate);
+    console.log(validFloorNumber);
+    console.log(validJob );
+    console.log(validBuildingNumber);
+    console.log(validCity);
+    console.log(validStreet);
 
     let customerData ="";
 
-    if(validName && validEmail && validBirthDate &&validFloorNumber &&validJob && validBuildingNumber && validStreet && validCity) {
+    if(validName && validBirthDate && validFloorNumber && validJob && validBuildingNumber && validStreet && validCity) {
 
 
-        let xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("PUT", '/shoppy/profile', true);
-        customerData = {
-            username: nameElement.value,
-            email: emailEelement.value,
-            dob: dateElement.value,
-            interests: interestsElement.value,
-            job:jopElement.value,
-            street:streetElement.value,
-            floorNumber:floorElement.value,
-            buildingNumber:buildingElement.value,
-            city:cityElement.value,
-            gender:genderElement.value
-        };
-        console.log(customerData);
-        xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        xmlHttp.send("customerData=" + JSON.stringify(customerData));
-        console.log(customerData);
-        submitButton.submit();
+        // let xmlHttp = new XMLHttpRequest();
+        // xmlHttp.open("POST", '/shoppy/profile', true);
+        // customerData = {
+        //     username: nameElement.value,
+        //     email: emailEelement.value,
+        //     dob: dateElement.value,
+        //     interests: interestsElement.value,
+        //     job:jopElement.value,
+        //     street:streetElement.value,
+        //     floorNumber:floorElement.value,
+        //     buildingNumber:buildingElement.value,
+        //     city:cityElement.value,
+        //     gender:genderElement.value
+        // };
+        // console.log(customerData);
+        // xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        // xmlHttp.send("customerData=" + JSON.stringify(customerData));
+        // console.log(customerData);
+        // submitButton.submit();
+        console.log("valid");
+        for (var pair of new FormData(form).entries()) {
+            console.log(pair[0] + ': ' + pair[1]);
+        }
+        form.submit();
     }
 })
 
