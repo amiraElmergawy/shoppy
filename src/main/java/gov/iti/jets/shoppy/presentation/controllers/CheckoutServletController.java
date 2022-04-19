715f2c549f1a2407c305910bbb2cdf4991d57893
amiraElmergawy <amiraelmergawy@gmail.com>
2022-04-19 06:27:39 +0200

    Create CheckoutServletController.java

---
+++ src/main/java/gov/iti/jets/shoppy/presentation/controllers/CheckoutServletController.java
@@ -0,0 +1,32 @@
+package gov.iti.jets.shoppy.presentation.controllers;
+
+import gov.iti.jets.shoppy.service.DomainFacade;
+import gov.iti.jets.shoppy.service.dtos.OrderDto;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@WebServlet(name = "CheckoutServletController", value = "/checkout")
+public class CheckoutServletController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        var currentCustomerSession = request.getSession(false);
+        var orderDto = (OrderDto)currentCustomerSession.getAttribute("cart");
+        if(orderDto != null){ //check for this in front before click the button
+            orderDto.setIsSubmitted(true);
+            System.out.println(orderDto);
+            if (orderDto.getOrderProducts().size() != 0)
+                if(!DomainFacade.getInstance().saveOrder(Optional.of(orderDto)))
+                    response.sendRedirect("shopping-cart?increaseCreditLimit=true");
+                else
+                    response.sendRedirect("shopping-cart?successOperation=true");
+            else
+                response.sendRedirect("shopping-cart?emptyCart=true");
+        }
+    }
+
+}
