1772a6a6b20bdc90be89da3dcdc00b3358542873
amiraElmergawy <amiraelmergawy@gmail.com>
2022-04-21 11:21:02 +0200

    Revert "Revert "Add category feature""
    This reverts commit b5e4d2ce7a18773d6ec4affc9cc278c5cc7cc017.

---
+++ src/main/webapp/assets/scripts/home.js
@@ -0,0 +1,8 @@
+function handleCategory(){
+
+    let category = document.querySelector('input[name="category"]:checked').value;
+    console.log("category "+category);
+
+    // $.post("home", {"category": category});
+
+}
